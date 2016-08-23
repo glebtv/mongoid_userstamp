7fbaa928bf5495c525f1652e4f2d756ae34a52b8
glebtv <glebtv@gmail.com>
2016-08-23 14:00:03 +0300

    Fix deprecation - replace before_filter with before_action

--- lib/mongoid/userstamp/railtie.rb
+++ lib/mongoid/userstamp/railtie.rb
@@ -2,7 +2,7 @@ module Mongoid
   module Userstamp
     class Railtie < Rails::Railtie
       ActiveSupport.on_load :action_controller do
-        before_filter do |c|
+        before_action do |c|
           unless Mongoid::Userstamp.config.user_model.respond_to? :current
             Mongoid::Userstamp.config.user_model.send(
               :include,
