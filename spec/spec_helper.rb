940c03999c1e81b30395cf490c0c36770a4dbbcd
glebtv <glebtv@gmail.com>
2016-09-28 15:59:11 +0300

    fix for newer mongoid

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,8 @@ Dir[File.expand_path('../support/**/*.rb', __FILE__)].each do |f|
   require f
 end
 
+Mongo::Logger.logger.level = ::Logger::FATAL
+
 RSpec.configure do |config|
   config.mock_with :rspec
   
