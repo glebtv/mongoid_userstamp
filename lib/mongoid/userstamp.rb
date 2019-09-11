523ca642a7d14357611f92720b7607da7d648fdf
Michael Watts <mikwat@users.noreply.github.com>
2019-09-11 14:21:02 -0700

    Explicitly define inverse_of nil on relations.

--- lib/mongoid/userstamp.rb
+++ lib/mongoid/userstamp.rb
@@ -3,8 +3,8 @@ module Mongoid
     extend ActiveSupport::Concern
 
     included do
-      belongs_to Userstamp.config.creator_field, class_name: Userstamp.config.user_model_name, autosave: false, optional: true
-      belongs_to Userstamp.config.updater_field, class_name: Userstamp.config.user_model_name, autosave: false, optional: true
+      belongs_to Userstamp.config.creator_field, class_name: Userstamp.config.user_model_name, autosave: false, optional: true, inverse_of: nil
+      belongs_to Userstamp.config.updater_field, class_name: Userstamp.config.user_model_name, autosave: false, optional: true, inverse_of: nil
 
       before_validation :set_updater
       before_validation :set_creator
